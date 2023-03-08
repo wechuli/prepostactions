3f330d0c219ffb7c10556f6b218c66ca16eb954c
Wechuli <wechulipaul@yahoo.com>
2023-03-08 14:57:31 +0300

    Update configure.js

--- bin/configure.js
+++ bin/configure.js
@@ -1,6 +1,7 @@
 const { execSync } = require("child_process");
 // check node version
 console.log(process.version);
+execSync(`yarn node --version`, { stdio: "inherit" });
 execSync('echo "Yarn version: `yarn --version`"', { stdio: "inherit" });
 execSync(`yarn --cwd "${__dirname}/../" install --prod  --non-interactive --no-progress`, { stdio: "inherit" });
 execSync(`yarn --cwd "${__dirname}/../" list --depth=0 --prod --non-interactive --no-progress`, { stdio: "inherit" });
